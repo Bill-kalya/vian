c82ad89599c9c6eda0224071faf4c2f4735d228c
Bill Kalya <kalyakiprono2003@gmail.com>
2025-11-28 11:36:31 +0300

    added its backend

---
+++ vian_backend/src/main/java/com/vian/VianBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VianBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VianBackendApplication.class, args);
+	}
+
+}
---
+++ vian_backend/src/test/java/com/vian/VianBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VianBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
